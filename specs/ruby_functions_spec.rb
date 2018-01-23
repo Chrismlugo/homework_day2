936982281f616cba5eee251fdc1ff42b8ce32d21
Chris Murphy <_chrism_@live.com>
2018-01-23 18:10:58 +0000

    code added to ruby_funtions_spec

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -70,17 +70,32 @@ class Functions_Practice < MiniTest::Test
 
   #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
-    #add test code here
+  #arrange
+
+  # act
+
+  #assert
+  assert_equal()
   end
 
   #Given the radius of a sphere calculate the volume
   def test_volume_of_sphere()
-    #add test code here
+  #arrange
+
+  #act
+
+  #assert
+  assert_equal()
   end
 
   #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
-    #add test code here
+  #arrange
+
+  #act
+
+  #assert
+  assert_equal()
   end
 
 
